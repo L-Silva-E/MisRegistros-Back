d7f7f21b0073a945c4c65ef479f03765f461382d
Lucas Silva <L.Silva.E@hotmail.com>
2024-05-29 20:27:04 -0400

    ✨ Added enums

---
+++ src/shared/enums.ts
@@ -0,0 +1,19 @@
+export enum Unit {
+  LITER = "l",
+  MILLILITER = "ml",
+  GRAM = "g",
+  KILOGRAM = "kg",
+  POUND = "lb",
+  OUNCE = "oz",
+  TEASPOON = "tsp",
+  TABLESPOON = "tbsp",
+}
+
+export enum Score {
+  VERY_BAD = 0,
+  BAD = 1,
+  OK = 2,
+  GOOD = 3,
+  GREAT = 4,
+  EXCELLENT = 5,
+}
