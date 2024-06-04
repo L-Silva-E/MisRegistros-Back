78b6279871bb115e01f3fdf815ded46d4ec3db9a
Lucas Silva <L.Silva.E@hotmail.com>
2024-06-04 00:38:51 -0400

    🌱 Added seed with ingredients

---
+++ src/shared/prisma/seed/data.ts
@@ -0,0 +1,100 @@
+import { Unit } from "../../enums";
+
+export const dataIngredients = [
+  {
+    name: "Tomate",
+    unit: Unit.UNIT,
+  },
+  {
+    name: "Cebolla",
+    unit: Unit.UNIT,
+  },
+  {
+    name: "Pimentón",
+    unit: Unit.UNIT,
+  },
+  {
+    name: "Ajo",
+    unit: Unit.UNIT,
+  },
+  {
+    name: "Aceite",
+    unit: Unit.MILLILITER,
+  },
+  {
+    name: "Vinagre",
+    unit: Unit.MILLILITER,
+  },
+  {
+    name: "Sal",
+    unit: Unit.GRAM,
+  },
+  {
+    name: "Pimienta",
+    unit: Unit.GRAM,
+  },
+  {
+    name: "Pan",
+    unit: Unit.UNIT,
+  },
+  {
+    name: "Huevo",
+    unit: Unit.UNIT,
+  },
+  {
+    name: "Lechuga",
+    unit: Unit.UNIT,
+  },
+  {
+    name: "Atún",
+    unit: Unit.GRAM,
+  },
+  {
+    name: "Zanahoria",
+    unit: Unit.UNIT,
+  },
+  {
+    name: "Pepino",
+    unit: Unit.UNIT,
+  },
+  {
+    name: "Fideos",
+    unit: Unit.GRAM,
+  },
+  {
+    name: "Queso",
+    unit: Unit.GRAM,
+  },
+  {
+    name: "Jamón",
+    unit: Unit.GRAM,
+  },
+  {
+    name: "Pollo",
+    unit: Unit.GRAM,
+  },
+  {
+    name: "Arroz",
+    unit: Unit.GRAM,
+  },
+  {
+    name: "Papas",
+    unit: Unit.UNIT,
+  },
+  {
+    name: "Carne de Vacuno",
+    unit: Unit.GRAM,
+  },
+  {
+    name: "Carne de Cerdo",
+    unit: Unit.GRAM,
+  },
+  {
+    name: "Pescado",
+    unit: Unit.GRAM,
+  },
+  {
+    name: "Marisco",
+    unit: Unit.GRAM,
+  },
+];
