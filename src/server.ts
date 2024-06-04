3d8838af65929755b51d88a645a9f38569e014b6
Lucas Silva <L.Silva.E@hotmail.com>
2024-06-03 21:18:49 -0400

    🚚 Renamed module 'Recipes' to 'RecipeBook'

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import app from "./shared/app";
 import environment from "./shared/environment";
 
 // ~ Routers
-import RecipeRouter from "./modules/recipes/routes/recipe.routes";
-import IngredientRouter from "./modules/recipes/routes/ingredient.routes";
+import RecipeRouter from "./modules/recipeBook/routes/recipe.routes";
+import IngredientRouter from "./modules/recipeBook/routes/ingredient.routes";
 
 async function init() {
   const version = environment.API_VERSION;
