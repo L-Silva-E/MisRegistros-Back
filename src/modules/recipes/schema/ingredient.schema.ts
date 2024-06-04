954cba7c38d7a88665dd78db066eef2bfdf30afc
Lucas Silva <L.Silva.E@hotmail.com>
2024-06-03 21:11:32 -0400

    🦺 Improvement with Base schema for 'Ingredients' model

--- src/modules/recipes/schema/ingredient.schema.ts
+++ src/modules/recipes/schema/ingredient.schema.ts
@@ -1,80 +1,39 @@
 import { z } from "zod";
+import {
+  PrimaryKeySchema,
+  GetZodSchema,
+} from "../../../shared/zod/schema/base.schema";
 
 import { Unit } from "../../../shared/enums";
 
-export const IngredientCreateZodSchema = z.object({
-  body: z.object({
-    name: z.string().min(1, "El campo 'nombre' no puede estar vacío"),
-    unit: z.nativeEnum(Unit, {
-      errorMap: () => {
-        return {
-          message:
-            "El campo 'unidad' debe ser uno de los siguientes tipos: " +
-            Object.values(Unit).join(", "),
-        };
-      },
-    }),
+// ~ Base Zod Squema
+export const IngredientBaseZodSchema = z.object({
+  name: z.string().min(1, "El campo 'nombre' no puede estar vacío"),
+  unit: z.nativeEnum(Unit, {
+    errorMap: () => {
+      return {
+        message:
+          "El campo 'unidad' debe ser uno de los siguientes tipos: " +
+          Object.values(Unit).join(", "),
+      };
+    },
   }),
+});
+
+// ~ CRUD Zod Schemas
+export const IngredientCreateZodSchema = z.object({
+  body: IngredientBaseZodSchema.strict(),
 });
 
 export const IngredientGetZodSchema = z.object({
-  query: z.object({
-    id: z.coerce
-      .number({ invalid_type_error: "La 'id' debe ser un número" })
-      .int()
-      .positive("Ingrese una 'id' válida")
-      .optional(),
-    name: z
-      .string()
-      .min(1, "El campo 'nombre' no puede estar vacío")
-      .optional(),
-    unit: z
-      .nativeEnum(Unit, {
-        errorMap: () => {
-          return {
-            message:
-              "El campo 'unidad' debe ser uno de los siguientes tipos: " +
-              Object.values(Unit).join(", "),
-          };
-        },
-      })
-      .optional(),
-    createdAt: z
-      .string()
-      .min(1, "El campo 'creado' no puede estar vacío")
-      .optional(),
-    updatedAt: z
-      .string()
-      .min(1, "El campo 'actualizado' no puede estar vacío")
-      .optional(),
-  }),
+  query: GetZodSchema.merge(IngredientBaseZodSchema.partial()).strict(),
 });
 
 export const IngredientUpdateZodSchema = z.object({
-  body: z.object({
-    name: z
-      .string()
-      .min(1, "El campo 'nombre' no puede estar vacío")
-      .optional(),
-    unit: z
-      .nativeEnum(Unit, {
-        errorMap: () => {
-          return {
-            message:
-              "El campo 'unidad' debe ser uno de los siguientes tipos: " +
-              Object.values(Unit).join(", "),
-          };
-        },
-      })
-      .optional(),
-  }),
+  params: PrimaryKeySchema.strict(),
+  body: IngredientBaseZodSchema.partial(),
 });
 
 export const IngredientDeleteZodSchema = z.object({
-  params: z.object({
-    id: z
-      .number({ invalid_type_error: "La 'id' debe ser un número" })
-      .int()
-      .positive(),
-  }),
+  params: PrimaryKeySchema.strict(),
 });
