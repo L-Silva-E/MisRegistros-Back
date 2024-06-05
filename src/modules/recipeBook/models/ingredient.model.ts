8f428265e015bfb1ef4eb946f4d30606882f5cb2
Lucas Silva <L.Silva.E@hotmail.com>
2024-06-04 23:25:17 -0400

    📝 Improvement in interfaces of 'Ingredient'

--- src/modules/recipeBook/models/ingredient.model.ts
+++ src/modules/recipeBook/models/ingredient.model.ts
@@ -1,5 +1,11 @@
-export default interface IngredientModel {
+export interface IngredientModel {
+  count?: number;
   id: number;
   name: string;
   unit: string;
+}
+
+export interface IngredientCountModel {
+  count: number;
+  ingredients: IngredientModel[];
 }
