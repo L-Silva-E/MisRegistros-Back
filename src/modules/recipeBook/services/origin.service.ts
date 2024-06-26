238c0149d1baa534f80fc3301c952affa3c98b6a
Lucas Silva <L.Silva.E@hotmail.com>
2024-06-25 22:35:10 -0400

    ✨ Added 'Origin' model

---
+++ src/modules/recipeBook/services/origin.service.ts
@@ -0,0 +1,76 @@
+import { PrismaClient } from "@prisma/client";
+import { Context } from "../../../shared/jest/context";
+import { getQueryOptions } from "../../../shared/prisma/utils/prisma.utils";
+import { OriginModel, OriginCountModel } from "../models/origin.model";
+
+const prismaClient = new PrismaClient();
+
+export default class OriginService {
+  public async create(
+    origin: OriginModel,
+    ctx?: Context
+  ): Promise<OriginModel> {
+    const prisma = ctx?.prisma || prismaClient;
+
+    try {
+      const originCreated = await prisma.origin.create({
+        data: origin,
+      });
+
+      return originCreated;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async get(query: any, ctx?: Context): Promise<OriginCountModel> {
+    const prisma = ctx?.prisma || prismaClient;
+
+    try {
+      const insensitiveFields = ["name"];
+      const queryOptions = getQueryOptions(query, insensitiveFields);
+
+      const [origins, count] = await prisma.$transaction([
+        prisma.origin.findMany(queryOptions),
+        prisma.origin.count(),
+      ]);
+
+      return { count, origins: origins };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async patch(
+    id: number,
+    origin: OriginModel,
+    ctx?: Context
+  ): Promise<OriginModel> {
+    const prisma = ctx?.prisma || prismaClient;
+
+    try {
+      const originUpdated = await prisma.origin.update({
+        where: { id },
+        data: origin,
+      });
+
+      return originUpdated;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async delete(id: number, ctx?: Context): Promise<OriginModel> {
+    const prisma = ctx?.prisma || prismaClient;
+
+    try {
+      const originDeleted = await prisma.origin.delete({
+        where: { id },
+      });
+
+      return originDeleted;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
