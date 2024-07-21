fa680851c3fd48488961b9d5b55b5dd939921edb
Lucas Silva <L.Silva.E@hotmail.com>
2024-07-20 22:15:22 -0400

    🗃️ Added 'Feature' model in Prisma

---
+++ src/shared/prisma/migrations/20240721020827_added_feature_model/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Feature" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Feature_pkey" PRIMARY KEY ("id")
+);
