f0f655d19764582aec90284852bf5382e28465ac
Lucas Silva <L.Silva.E@hotmail.com>
2025-02-14 20:02:06 -0300

    📝 Changed limit and page, now returns all data

--- src/shared/prisma/utils/prisma.utils.ts
+++ src/shared/prisma/utils/prisma.utils.ts
@@ -70,7 +70,11 @@ export const getOrder = (query: QueryParams) => {
 };
 
 export const getPagination = (query: QueryParams) => {
-  const take = query.limit ? +query.limit : 10;
+  if (!('limit' in query) && !('page' in query)) {
+    return {};
+  }
+
+  const take = query.limit ? +query.limit : undefined;
   const skip = query.page && query.limit ? query.page * query.limit : 0;
 
   delete query.limit;
