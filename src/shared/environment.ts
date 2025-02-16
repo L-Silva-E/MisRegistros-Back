ceae50bcf36fe8e126a8b32a39e078d436f6db4c
Lucas Silva <L.Silva.E@hotmail.com>
2025-02-16 15:27:40 -0300

    🐛 Fixed problem loading env vars from file

--- src/shared/environment.ts
+++ src/shared/environment.ts
@@ -1,9 +1,11 @@
 import { IformatEnvironments } from "./types.environment";
+import dotenv from "dotenv";
+dotenv.config();
 
 const environments: IformatEnvironments = {
   API_KEY: process.env.API_KEY || "MISSING_API_KEY",
   API_URL: process.env.API_URL || "http://localhost:9000",
-  API_PORT: parseInt(process.env.API_PORT || "9000"),
+  API_PORT: Number(process.env.API_PORT) || 9000,
   API_VERSION: process.env.API_VERSION || "v1",
 
   FRONT_URL_LOCAL: process.env.FRONT_URL_LOCAL || "http://localhost:5173",
@@ -11,7 +13,7 @@ const environments: IformatEnvironments = {
   FRONT_URL_PROD: process.env.FRONT_URL_PROD || "",
 
   DB_HOST: process.env.DB_HOST || "localhost",
-  DB_PORT: parseInt(process.env.DB_PORT || "5432"),
+  DB_PORT: Number(process.env.DB_PORT) || 5432,
   DB_USERNAME: process.env.DB_USERNAME || "postgres",
   DB_PASSWORD: process.env.DB_PASSWORD || "postgres",
   DB_DATABASE: process.env.DB_DATABASE || "postgres",
--- src/shared/prisma/middlewares/authorization.ts
+++ src/shared/prisma/middlewares/authorization.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import IResponse from "../../../shared/interfaces/Iresponse";
+import environment from "../../environment";
 
 export default function AuthorizationApiKey(
   req: Request,
@@ -8,7 +9,7 @@ export default function AuthorizationApiKey(
 ): Response | void {
   const apiKey = req?.get("api-key");
 
-  if (!apiKey || apiKey !== process.env.API_KEY) {
+  if (!apiKey || apiKey !== environment.API_KEY) {
     const apiError: IResponse = {
       code: 401,
       message: "Unauthorized",
