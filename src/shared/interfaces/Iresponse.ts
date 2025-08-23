47ff253f23fd7c289756b7d8dce07080ab2ac956
Lucas Silva <L.Silva.E@hotmail.com>
2025-08-23 00:15:12 -0400

    🗑️ chore: Remove unused interfaces IBaseModel, IBaseRepository, and IResponse

--- src/shared/interfaces/Iresponse.ts
+++
@@ -1,9 +0,0 @@
-import { HttpStatusCode } from "../types.environment";
-
-export default interface IResponse {
-  code: HttpStatusCode;
-  message: string;
-  count?: number;
-  data: any;
-  stackError?: any;
-}
